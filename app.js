19d3c8d0ad13f716ede7b73f9f065e5bcaea6580
RJ Robinson <robertj.robinson@gmail.com>
2015-06-28 17:47:00 -0400

    handled HTTP requests. "

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [
+    'ngRoute'
+]);
 
-myApp.controller('mainController', ['$scope',
-    function($scope) {
+
+myApp.config(function($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+});
+
+myApp.service('nameService', function() {
+
+    var self = this;
+    this.name = "RJ Robinson";
+
+    this.nameLength = function() {
+        return self.name.length
+    };
+
+})
+
+// CONTROLLERS
+myApp.controller('mainController', ['$scope', '$log', 'nameService',
+
+    function($scope, $log, nameService) {
+        // Controller
+        $scope.$watch('name', function() {
+            nameService.name = $scope.name
+        })
+
+        $scope.name = nameService.name;
+
+    }
+]);
+
+
+myApp.controller('secondController', ['$scope', '$log', 'nameService',
+
+    function($scope, $log, nameService) {
         // Controller
 
-        $scope.handle = '';
+        $scope.name = nameService.name;
+
+        $scope.$watch('name', function() {
+            nameService.name = $scope.name
+        })
+
     }
 ]);
